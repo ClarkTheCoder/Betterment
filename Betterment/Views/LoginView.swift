1d45de389c7205d0cd104dff208d794bc492936b
Carson Clark <carsonclark@Carsons-MacBook-Air.local>
2024-08-26 09:50:12 -0700

    add textfield for login

--- Betterment/Views/LoginView.swift
+++ Betterment/Views/LoginView.swift
@@ -8,14 +8,32 @@
 import SwiftUI
 
 struct LoginView: View {
+    @State private var email = ""
+    @State private var password = ""
+    
     var body: some View {
         ZStack {
             Color.black
+                .ignoresSafeArea()
+         
             RoundedRectangle(cornerRadius: 30, style: .continuous)
                 .foregroundStyle(Color.white)
                 .frame(width: 1000, height: 400)
+                .rotationEffect(.degrees(135))
+                .offset(y: -350)
+            
+            VStack(spacing: 20){
+                Text("Welcome")
+                    .foregroundStyle(Color.black)
+                    .bold()
+                    .font(.largeTitle)
+                    .offset(x: -100, y: -100)
+                
+                TextField("Email", text: $email)
+                    .foregroundStyle(Color.white)
+                    .textFieldStyle(.plain)
+            }
         }
-        .ignoresSafeArea()
     }
 }
 
